7f6a8c4c9e2f13dcc7128ef9c14defcb9f8ff522
JoelDsouza <joeldsouza8@gmail.com>
2024-12-08 18:52:38 +0000

    added  implementation for the 'is_palindrome' method

---
+++ Linked_List/Double_Linked_List/Leetcode/is_palindrome.py
@@ -0,0 +1,94 @@
+﻿class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    def __str__(self):
+        return f"Node({self.value})"
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def __str__(self):
+        values = []
+        temp = self.head
+        while temp is not None:
+            values.append(str(temp.value))
+            temp = temp.next
+        return " <-> ".join(values)
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def is_palindrome(self):
+        # Check if there are any nodes in the list, as an empty list reads the same forwards and backwards so is a palindrome
+        if self.head is None:
+            return True
+
+        if self.length == 1:
+            return True
+
+        forward = self.head
+        backward = self.tail
+
+        index = self.length // 2
+
+        for _ in range(index):
+            if forward.value == backward.value:
+                forward = forward.next
+                backward = backward.prev
+
+            else:
+                return False
+
+        return True
+
+
+my_dll_1 = DoublyLinkedList(1)
+my_dll_1.append(2)
+my_dll_1.append(3)
+my_dll_1.append(2)
+my_dll_1.append(1)
+
+print('my_dll_1 is_palindrome:')
+print(my_dll_1.is_palindrome())
+
+my_dll_2 = DoublyLinkedList(1)
+my_dll_2.append(2)
+my_dll_2.append(3)
+
+print('\nmy_dll_2 is_palindrome:')
+print(my_dll_2.is_palindrome())
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    my_dll_1 is_palindrome:
+    True
+
+    my_dll_2 is_palindrome:
+    False
+
+"""
+
