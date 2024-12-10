c79817c9d81c104533f83b616591739c649f28e4
JoelDsouza <joeldsouza8@gmail.com>
2024-12-10 19:47:16 +0000

    added  implementation for the 'enqueue' method for queues

---
+++ Linked_List/Queues/Practice/enqueue.py
@@ -0,0 +1,55 @@
+﻿class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def enqueue(self, value: int):
+        new_node = Node(value)
+
+        # Check if there are any nodes in the queue
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+
+        else:
+            self.last.next = new_node
+            self.last = new_node
+
+        self.length += 1
+
+
+my_queue = Queue(1)
+
+print('Queue before enqueue(2):')
+my_queue.print_queue()
+
+my_queue.enqueue(2)
+
+print('\nQueue after enqueue(2):')
+my_queue.print_queue()
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Queue before enqueue(2):
+    1
+
+    Queue after enqueue(2):
+    1
+    2
+
+"""
