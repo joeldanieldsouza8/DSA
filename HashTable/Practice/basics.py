ff55dcdb07379ffd357e8fd8d37e2ec612fc37e1
JoelDsouza <joeldsouza8@gmail.com>
2024-12-16 19:57:14 +0000

    added  implementation for the 'constructor' method for HashTable

---
+++ HashTable/Practice/basics.py
@@ -0,0 +1,43 @@
+﻿my_hash_set = [
+    [None],
+    ['Jones'],
+    [None],
+    ['Lisa', 'Stuart'],
+    [None],
+    ['Bob'],
+    [None],
+    ['Siri'],
+    ['Pete'],
+    [None]
+]
+
+
+# def hash_function(name: str):
+#     sum_of_chars = 0
+#
+#     for char in name:
+#         sum_of_chars += ord(char)
+#
+#     return sum_of_chars % 10
+
+def hash_function(name: str):
+    return sum(ord(char) for char in name) % 10
+
+def add(value):
+    index = hash_function(value)
+    bucket = my_hash_set[index]
+
+    if value not in bucket:
+        bucket.append(value)
+
+def contains(value):
+    index = hash_function(value)
+    bucket = my_hash_set[index]
+
+    return value in bucket
+
+add('Stuart')
+add('Stuart')
+
+print(my_hash_set)
+print('Contains Stuart:',contains('Stuart'))
