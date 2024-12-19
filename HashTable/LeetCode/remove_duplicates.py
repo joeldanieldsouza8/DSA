8c82d01b71e63d033a0eaee6d3aed70dc7c1f217
JoelDsouza <joeldsouza8@gmail.com>
2024-12-19 19:19:59 +0000

    added implementation for the 'remove_duplicates' method

---
+++ HashTable/LeetCode/remove_duplicates.py
@@ -0,0 +1,29 @@
+﻿# Approach 1: Using a for loop which is unnecessary
+# def remove_duplicates(nums: list[int]):
+#     unique = set()
+#
+#     for index, num in enumerate(nums):
+#         if num not in unique:
+#             unique.add(num)
+#
+#     return list(unique)
+
+# Approach 2: Directly passing the list into the set
+def remove_duplicates(nums: list[int]):
+    unique_list = list(set(nums))
+    return unique_list
+
+my_list = [1, 2, 3, 4, 1, 2, 5, 6, 7, 3, 4, 8, 9, 5]
+new_list = remove_duplicates(my_list)
+print(new_list)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    (Order may be different as sets are unordered)
+
+"""
