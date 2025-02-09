779ff4c9826b99efb8e6e90fa1e4e160d96ca39f
JoelDsouza <joeldsouza8@gmail.com>
2025-02-09 14:45:15 +0000

    added implementation for 'selection_sort' method for sorting algorithm

---
+++ Sorting/Practice/selection_sort.py
@@ -0,0 +1,30 @@
+﻿def selection_sort(nums: list[int]):
+    n = len(nums)
+
+    # Iterate through the list, moving the boundary of the sorted section
+    for i in range(n - 1):
+        # Assume the first element in the unsorted section is the minimum
+        min_index = i
+
+        # With each pass, the previous index position contains the sorted element. So, we don't need to go through the entire list again. Hence, iterate over only the remaining unsorted elements to find the smallest value
+        for j in range(i + 1, n):
+            # Update min_index if a smaller element is found
+            if nums[j] < nums[min_index]:
+                min_index = j
+
+        # Swap only if a smaller element was found
+        if min_index != i:
+            # After a given pass through part of the list is done, swap the element at the current index position with the element at the 'min_index'
+            nums[i], nums[min_index] = nums[min_index], nums[i]
+
+    return nums
+
+
+print(selection_sort([4, 2, 6, 5, 1, 3]))
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6]
+
+ """
