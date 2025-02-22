d648e03ce4092b8388cb1b8f1394cf2b28830430
JoelDsouza <joeldsouza8@gmail.com>
2025-02-22 17:54:55 +0000

    added implementation for 'selection_sort' in single-linked list

---
+++ Sorting/Leetcode/selection_sort_linked_list.py
@@ -0,0 +1,102 @@
+﻿class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return f"Node({self.value})"
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def __str__(self):
+        result = []
+        temp = self.head
+        while temp:
+            result.append(str(temp.value))
+            temp = temp.next
+        return " -> ".join(result) + f" | Length: {self.length}"
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def selection_sort(self):
+        # Check if the linked list is empty or contains only one node
+        if self.head is None or self.length == 1:
+            return self
+
+        n = self.length
+        curr_node = self.head # Represents the node to be swapped in the current pass through the list
+
+        for i in range(n - 1):
+            min_node = curr_node  # Assume curr_node is the smallest
+            temp_node = curr_node.next
+
+            # Traverse the unsorted section of the list
+            while temp_node:
+                # Update min_node if a smaller element is found
+                if temp_node.value < min_node.value:
+                    min_node = temp_node
+
+                temp_node = temp_node.next
+
+            # Swap only if a smaller element was found
+            if min_node != curr_node:
+                curr_node.value, min_node.value = min_node.value, curr_node.value
+
+            curr_node = curr_node.next
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.selection_sort()
+
+print("\nSorted Linked List:")
+my_linked_list.print_list()
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Linked List Before Sort:
+    4
+    2
+    6
+    5
+    1
+    3
+
+    Sorted Linked List:
+    1
+    2
+    3
+    4
+    5
+    6
+
+"""
--- Sorting/Practice/selection_sort.py
+++ Sorting/Practice/selection_sort.py
@@ -2,20 +2,20 @@
     n = len(nums)
 
     # Iterate through the list, moving the boundary of the sorted section
-    for i in range(n - 1):
+    for i_index in range(n - 1):
         # Assume the first element in the unsorted section is the minimum
-        min_index = i
+        min_index = i_index
 
         # With each pass, the previous index position contains the sorted element. So, we don't need to go through the entire list again. Hence, iterate over only the remaining unsorted elements to find the smallest value
-        for j in range(i + 1, n):
+        for j_index in range(i_index + 1, n):
             # Update min_index if a smaller element is found
-            if nums[j] < nums[min_index]:
-                min_index = j
+            if nums[j_index] < nums[min_index]:
+                min_index = j_index
 
         # Swap only if a smaller element was found
-        if min_index != i:
+        if min_index != i_index:
             # After a given pass through part of the list is done, swap the element at the current index position with the element at the 'min_index'
-            nums[i], nums[min_index] = nums[min_index], nums[i]
+            nums[i_index], nums[min_index] = nums[min_index], nums[i_index]
 
     return nums
 
