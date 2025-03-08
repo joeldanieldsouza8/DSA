a38e94cdfcadc02a4cad7103f401bd4cf0415b70
JoelDsouza <joeldsouza8@gmail.com>
2025-03-08 14:48:15 +0000

    added implementation for 'insertion_sort'

--- Sorting/Practice/insertion_sort.py
+++ Sorting/Practice/insertion_sort.py
@@ -1,18 +1,18 @@
 ﻿def insertion_sort(nums: list[int]):
     n = len(nums)
 
-    # Iterate through the list starting from the second element
+    # Iterate through the list starting from the second element. The assumption is that the first element at i=0 is already sorted
     for i in range(1, n):
         key = nums[i]  # Element to be inserted into the sorted portion
-        j_index = i - 1  # Start comparing with the previous element
+        j = i - 1  # Start comparing with the previous element
 
-        # Shift elements to the right until the correct position for key is found
-        while j_index >= 0 and nums[j_index] > key:
-            nums[j_index + 1] = nums[j_index]  # Shift element one step to the right
-            j_index -= 1  # Move left in the list
+        # Shift elements to the right until the correct position for the key is found
+        while j >= 0 and nums[j] > key:
+            nums[j + 1] = nums[j]  # Shift element one step to the right
+            j -= 1  # Move left in the list
 
         # Place the key at its correct position
-        nums[j_index + 1] = key
+        nums[j + 1] = key
 
     return nums
 
