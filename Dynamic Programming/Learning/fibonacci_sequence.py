4af417a8eb83c65d61a7bd30d98bb205ef8308fa
Joel D'Souza <joeldsouza8@gmail.com>
2026-01-25 15:34:59 +0000

    Added implementation for the bottom-up approach for the fibonacci_sequece iterative.

--- Dynamic Programming/Learning/fibonacci_sequence.py
+++ Dynamic Programming/Learning/fibonacci_sequence.py
@@ -7,19 +7,48 @@
 
 #     return result
 
-memo: dict[int, int] = {}
+
+# ---------------------------------------------------------------------------------------
+
+# Top-Down Approach
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+
+# memo: dict[int, int] = {}
+
+# def fibonacci_sequence(nth_term: int) -> int:
+#     # Check if the computation for the n-th term already exists in the dictionary
+#     if nth_term in memo:
+#         return memo[nth_term]
+
+#     if nth_term <= 1:
+#         return nth_term
+    
+#     result = fibonacci_sequence(nth_term - 1) + fibonacci_sequence(nth_term - 2)
+
+#     # Update the dictionary to store the computed value of the current nth term
+#     memo[nth_term] = result
+
+#     return result
+
+
+# ---------------------------------------------------------------------------------------
+
+# Bottom-Up Approach
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
 
 def fibonacci_sequence(nth_term: int) -> int:
-    # Check if the computation for the n-th term already exists in the dictionary
-    if nth_term in memo:
-        return memo[nth_term]
+    memo: dict[int, int] = {
+        0: 0,
+        1: 1
+    }
 
-    if nth_term <= 1:
-        return nth_term
-    
-    result = fibonacci_sequence(nth_term - 1) + fibonacci_sequence(nth_term - 2)
+    # Note, the `range()` function includes the 'start' number, but excludes the 'stop' number. It goes up to, but does not include, the limit. Hence, why the value for the 'stop' parameter is 'n + 1'
+    for i in range(2, nth_term + 1):
+        # Old Way: result = fibonacci_sequence(nth_term - 1) + fibonacci_sequence(nth_term - 2)
+        result = memo[nth_term - 1] + memo[nth_term - 2]
+        
+        memo[i] = result
 
-    # Update the dictionary to store the computed value of the current nth term
-    memo[nth_term] = result
-
-    return result
+    return memo[nth_term]
